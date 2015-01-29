3213f366b33e3d23ec1c2cd467ee3334d312efc1
kaivonafsari <kaivon.afsari@gmail.com>
2015-01-28 16:51:51 -0800

    hopefully last commit

--- koans/AboutArrays.js
+++ koans/AboutArrays.js
@@ -1,9 +1,9 @@
 describe("About Arrays", function() {
 
-  //We shall contemplate truth by testing reality, via spec expectations.  
+  //We shall contemplate truth by testing reality, via spec expectations.
   it("should create arrays", function() {
     var emptyArray = [];
-    expect(typeof(emptyArray)).toBe('object'); //A mistake? - http:javascript.crockford.com/remedial.html
+    expect(typeof(emptyArray)).toBe('object'); //A mistake? - http://javascript.crockford.com/remedial.html
     expect(emptyArray.length).toBe(0);
 
     var multiTypeArray = [0, 1, "two", function () { return 3; }, {value1: 4, value2: 5}, [6, 7]];
@@ -18,25 +18,25 @@ describe("About Arrays", function() {
   it("should understand array literals", function () {
     var array = [];
     expect(array).toEqual([]);
-    
+
     array[0] = 1;
     expect(array).toEqual([1]);
-    
+
     array[1] = 2;
     expect(array).toEqual([1, 2]);
-    
+
     array.push(3);
-    expect(array).toEqual([1, 2, 3]]);
+    expect(array).toEqual([1, 2, 3]);
   });
 
   it("should understand array length", function () {
     var fourNumberArray = [1, 2, 3, 4];
 
-    expect(fourNumberArray.length).toBe(4;
+    expect(fourNumberArray.length).toBe(4);
     fourNumberArray.push(5, 6);
     expect(fourNumberArray.length).toBe(6);
 
-    var tenEmptyElementArray = new Array(10); 
+    var tenEmptyElementArray = new Array(10);
     expect(tenEmptyElementArray.length).toBe(10);
 
     tenEmptyElementArray.length = 5;
@@ -45,13 +45,13 @@ describe("About Arrays", function() {
 
   it("should slice arrays", function () {
     var array = ["peanut", "butter", "and", "jelly"];
-    
+
     expect(array.slice(0, 1)).toEqual(["peanut"]);
     expect(array.slice(0, 2)).toEqual(["peanut", "butter"]);
     expect(array.slice(2, 2)).toEqual([]);
-    expect(array.slice(2, 20)).toEqual("and", "jelly");
+    expect(array.slice(2, 20)).toEqual(["and", "jelly"]);
     expect(array.slice(3, 0)).toEqual([]);
-    expect(array.slice(3, 100)).toEqual("jelly");
+    expect(array.slice(3, 100)).toEqual(["jelly"]);
     expect(array.slice(5, 1)).toEqual([]);
   });
 
@@ -59,14 +59,14 @@ describe("About Arrays", function() {
     var array = [ "zero", "one", "two", "three", "four", "five" ];
 
     function passedByReference(refArray) {
-      refArray[1] = "changed in function";
+        refArray[1] = "changed in function";
     }
     passedByReference(array);
     expect(array[1]).toBe("changed in function");
 
     var assignedArray = array;
     assignedArray[5] = "changed in assignedArray";
-    expect(array[5]).toBe("changed inassignedArray");
+    expect(array[5]).toBe("changed in assignedArray");
 
     var copyOfArray = array.slice();
     copyOfArray[3] = "changed in copyOfArray";
@@ -78,7 +78,7 @@ describe("About Arrays", function() {
     array.push(3);
 
     expect(array).toEqual([1, 2, 3]);
-    
+
     var poppedValue = array.pop();
     expect(poppedValue).toBe(3);
     expect(array).toEqual([1, 2]);
@@ -89,9 +89,9 @@ describe("About Arrays", function() {
 
     array.unshift(3);
     expect(array).toEqual([3, 1, 2]);
-    
+
     var shiftedValue = array.shift();
-    expect(shiftedValue).toEqual([3]);
+    expect(shiftedValue).toEqual(3);
     expect(array).toEqual([1, 2]);
-  });  
+  });
 });
